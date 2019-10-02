9ca52bd70d3ad6109a04a0e836acd5dc049afb81
Vladimir Lukyanov <vladimir@liikyanov.ru>
2019-10-02 16:15:58 +0300

    v0.0.12 – example.php file added

---
+++ example.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * An example file demonstrating how to add all controls.
+ *
+ * @package     WordPress-Metabox-Fields
+ * @category    Core
+ * @author      Vladimir Lukyanov (@nulllogic)
+ * @copyright   Copyright (c) 2019, Vladimir Lukyanov (@nulllogic)
+ * @license     https://opensource.org/licenses/MIT
+ * @since       1.0.0
+ */
+
